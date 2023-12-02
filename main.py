07dde6d7730c6c53d97069b9e03f9a7729e40efa
Uros Pantelic <90989221+pantela002@users.noreply.github.com>
2023-12-02 23:47:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,48 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 from streamlit_drawable_canvas import st_canvas
-
+import torch.nn as nn
+import torch.nn.functional as F
 import torch
 from torchvision import transforms
 from PIL import Image
 import torch.nn.functional as F
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        # 6 input image channel, 16 output channels, 5x5 square convolution kernel
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        # an affine operation: y = Wx + b
+        self.fc2 = nn.Linear(120, 84)
+        # an affine operation: y = Wx + b
+        self.fc3 = nn.Linear(84, 10)
 
-from reinforceNET import ConvNet
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # All dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
-import matplotlib.pyplot as plt
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        # Flatten the tensor
+        x = x.view(-1, self.num_flat_features(x))
+        # Apply relu function
+        x = F.relu(self.fc1(x))
+        # Apply relu function
+        x = F.relu(self.fc2(x))
+        # Apply softmax function
+        x = self.fc3(x)
+        return x
+
 
 model_code = """
 import tensorflow as tf
